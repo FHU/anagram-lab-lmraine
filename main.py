d760aa51e216d3f4ae0dc6c5410e8da97c6cbce4
lmraine <lraine@fhu.edu>
2024-02-09 09:22:47 -0600

    tests json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #REMOVE PASS AND FIX THIS FUNCTION
+#tests
 letterCount1 = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0,
                'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0,
                'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0,
